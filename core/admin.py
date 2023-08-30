3daf4fb22da7aea0828c94ed04365caf8b258890
verafadeeva <lunatik.yar@gmail.com>
2023-08-30 11:41:28 +0300

    feat(admin.py): Add set to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from core import models
+
+
+@admin.register(models.Performence)
+class PerformenceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', )
+    search_fields = ('name__startswith', )
+
+
+@admin.register(models.Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'release_year')
+
+
+@admin.register(models.Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'order_num')
